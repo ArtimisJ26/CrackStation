369f84b830dfdb936aec982be82e878712fe9a66
ArtimisJ26 <77852899+ArtimisJ26@users.noreply.github.com>
2022-10-29 13:21:47 -0700

    Added lookup file as dictionary in swift and implemented test case

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -1,6 +1,16 @@
-public struct CrackStation {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public class CrackStation {
+    static func loadDictionaryFromDisk() throws -> [String : String] {
+        guard let path = Bundle.module.url(forResource: "hash_dictionary", withExtension: "json") else { return [:] }
+
+        let data = try Data(contentsOf: path)
+        let jsonResult = try JSONSerialization.jsonObject(with: data)
+
+        if let lookupTable: Dictionary = jsonResult as? Dictionary<String, String> {
+            return lookupTable
+        } else {
+            return [:]
+        }
     }
 }
--- Tests/CrackStationTests/CrackStationTests.swift
+++ Tests/CrackStationTests/CrackStationTests.swift
@@ -2,10 +2,15 @@ import XCTest
 @testable import CrackStation
 
 final class CrackStationTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CrackStation().text, "Hello, World!")
+    func testLoadingLookupTableFromDisk() throws {
+        //When
+        //let crackStation = CrackStation.CrackStation()
+        let lookupTable = try CrackStation.loadDictionaryFromDisk()
+        let answer = lookupTable["kJ"]
+        //print(lookupTable)
+        //print(answer)
+        //Then
+        XCTAssertEqual(answer, "ebf46618066c3b6fca4dd4d020f99849da52b8de")
+        
     }
 }
