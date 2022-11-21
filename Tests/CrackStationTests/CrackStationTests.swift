b02fcaec03531a074b63dbaa34f50fb21c80decd
ArtimisJ26 <77852899+ArtimisJ26@users.noreply.github.com>
2022-11-20 18:31:27 -0800

    loaded both sha1 and sha2 in one file

--- Tests/CrackStationTests/CrackStationTests.swift
+++ Tests/CrackStationTests/CrackStationTests.swift
@@ -14,7 +14,7 @@ final class CrackStationTests: XCTestCase {
         
     }
 
-    func testdecryptA() throws {
+    func testdecryptSHA1A() throws {
         let shaHash = "ebf46618066c3b6fca4dd4d020f99849da52b8de"
         let crackStationInstance = CrackStation()
         let answer = try crackStationInstance.decrypt(shaHash: shaHash)
@@ -24,8 +24,39 @@ final class CrackStationTests: XCTestCase {
         XCTAssertEqual(answer, "kJ")
     }
 
-    func testdecryptB() throws {
+    func testdecryptSHA1B() throws {
         let shaHash = "41f8fc99dc7f46f151d746638620059170c45a03"
+        let crackStationInstance = CrackStation()
+        let answer = try crackStationInstance.decrypt(shaHash: shaHash)
+
+        //print(shaHash, answer)
+
+        XCTAssertEqual(answer, "zgf")
+    }
+
+
+    func testdecryptSHA2A() throws {
+        let shaHash = "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b"
+        let crackStationInstance = CrackStation()
+        let answer = try crackStationInstance.decrypt(shaHash: shaHash)
+
+        //print(shaHash, answer)
+
+        XCTAssertEqual(answer, "1")
+    }
+
+    func testdecryptSHA2B() throws {
+        let shaHash = "46874106f9ac7f3b68a422895aa223e1ea5ed0db860f179f6362cb166c65f8ca"
+        let crackStationInstance = CrackStation()
+        let answer = try crackStationInstance.decrypt(shaHash: shaHash)
+
+        //print(shaHash, answer)
+
+        XCTAssertEqual(answer, "kj")
+    }
+
+    func testdecryptSHA2C() throws {
+        let shaHash = "6dd7239dfad7763ca7310efdf6c0459c5a8169b80ef3f6ee772a269062a96775"
         let crackStationInstance = CrackStation()
         let answer = try crackStationInstance.decrypt(shaHash: shaHash)
 
--- generate_hash.py
+++ generate_hash.py
@@ -1,46 +1,3 @@
-# import hashlib
-# import string
-# import json
-
-# def writeToJSONFile(path, fileName, data):
-#     filePathNameWExt = './' + path + '/' + fileName + '.json'
-#     with open(filePathNameWExt, 'w') as fp:
-#         json.dump(data, fp)
-
-
-# path = './Sources/CrackStation'
-# fileName = 'sha1_hash_dictionary'
-
-# data = {}
-
-# for i in range(48, 123):
-#     if (i not in range(58,65)) and (i not in range(91,97)):
-#         #print(chr(i), end = "\n")
-#         s = str(chr(i))
-#         result = hashlib.sha1(s.encode())
-#         hashValue = result.hexdigest()
-#         data[hashValue] = s
-#         #print(s, hashValue)
-#         for j in range(48, 123):
-#             if (j not in range(58,65)) and (j not in range(91,97)):
-#                 a = str(chr(i)+chr(j))
-#                 #print(chr(i),chr(j))
-#                 #print(a, end=" ")
-#                 result = hashlib.sha1(a.encode())
-#                 hashValue = result.hexdigest()
-#                 #print(a, hashValue)
-#                 data[hashValue] = a
-
-# writeToJSONFile(path, fileName, data)
-
-
-
-
-
-
-
-
-
 import hashlib
 import string
 import json
@@ -52,11 +9,9 @@ def writeToJSONFile(path, fileName, data):
 
 
 path = './Sources/CrackStation'
-fileName1 = 'sha1_hash_dictionary'
-fileName2 = 'sha2_hash_dictionary'
+fileName1 = 'hash_dictionary'
 
 SHA1data = {}
-SHA2data = {}
 
 for i in range(48, 123):
     if (i not in range(58,65)) and (i not in range(91,97)):
@@ -67,7 +22,9 @@ for i in range(48, 123):
         SHA1data[hashValue1] = s
         result2 = hashlib.sha256(s.encode())
         hashValue2 = result2.hexdigest()
-        SHA2data[hashValue2] = s
+        SHA1data[hashValue2] = s
+        #print(s, hashValue1)
+        #print(s, hashValue2)
         #print(s, hashValue)
         for j in range(48, 123):
             if (j not in range(58,65)) and (j not in range(91,97)):
@@ -79,8 +36,9 @@ for i in range(48, 123):
                 SHA1data[hashValue1] = a
                 result2 = hashlib.sha256(a.encode())
                 hashValue2 = result2.hexdigest()
-                SHA2data[hashValue2] = a
-                #print(a, hashValue)
+                SHA1data[hashValue2] = a
+                #print(a, hashValue1)
+                #print(a, hashValue2)
             for k in range(48, 123):
                 if (j not in range(58,65)) and (j not in range(91,97)):
                     b = str(chr(i)+chr(j)+chr(k))
@@ -89,8 +47,8 @@ for i in range(48, 123):
                     SHA1data[hashValue1] = b
                     result2 = hashlib.sha256(b.encode())
                     hashValue2 = result2.hexdigest()
-                    SHA2data[hashValue2] = b
-                    #print(b, hashValue)
+                    SHA1data[hashValue2] = b
+                    #print(b, hashValue1)
+                    #print(b, hashValue2)
 
 writeToJSONFile(path, fileName1, SHA1data)
-writeToJSONFile(path, fileName2, SHA2data)
